3ceb39aaf2c4bcbeaa510ef07c049feb93835c92
JoonasPitk <46161@edu.raseko.fi>
2022-10-03 10:26:45 +0300

    Uloskirjaus, settings.

--- varaukset/settings.py
+++ varaukset/settings.py
@@ -123,9 +123,10 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+
 ACCOUNT_ACTIVATION_DAYS = 1
 REGISTRATION_OPEN = True
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
-LOGIN_REDIRECT_URL = '/'
